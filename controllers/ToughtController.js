f0c8a8ee732b9cf724bdaec573b23d02a0321595
Jefferson Lira <jefferson.senac1@yahoo.com>
2022-07-26 19:50:14 -0300

    mostrando as frases na dashboard

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -2,9 +2,17 @@ const Tought = require('../models/Tought')
 const User = require('../models/User')
 
 module.exports = class ToughtController{
+
   static async showToughts(req, res) {
-    res.render('toughts/home')
+    const toughtsData = await Tought.findAll({
+      include: User,
+    })
+    const toughts = toughtsData.map((result)=> result.get({plain: true}))
+    res.render('toughts/home', {toughts})
   }
+
+
+
   static async dashboard(req,res) {
     const userId = req.session.userid
     const user = await User.findOne({
@@ -22,7 +30,16 @@ module.exports = class ToughtController{
 
     const toughts = user.Toughts.map((result)=> result.dataValues) // pegando só o pensamento do user
 
-    res.render('toughts/dashboard', { toughts })
+    let emptyToughts = true
+
+    if (toughts.length > 0) {
+      emptyToughts = false
+    }
+
+    //console.log(toughts)
+    //console.log(emptyToughts)
+
+    res.render('toughts/dashboard', { toughts, emptyToughts })
   }
 
   static createTought(req, res) {
@@ -60,4 +77,38 @@ module.exports = class ToughtController{
       })
       .catch((err) => console.log())
   }
+
+  static async updateTought(req, res){
+    const id = req.params.id
+    const tought = await Tought.findOne({where: {id: id}, raw: true})
+    res.render('toughts/edit', { tought })
+  }
+
+  static updateToughtPost(req, res) {
+    const id = req.body.id
+
+    const tought = {
+      title: req.body.title,
+      description: req.body.description,
+    }
+}
+
+static updateToughtPost(req, res) {
+  const id = req.body.id
+  const tought = {
+    title: req.body.title,
+    description: req.body.description,
+  }
+
+  Tought.update(tought, { where: {id: id}})
+    .then(()=>{
+      req.flash('message', 'Pensamento atualizado com sucesso!')
+      req.session.save(()=>{
+        res.redirect('/toughts/dashboard')
+      })
+    })
+    .catch((err) => console.log())
+
+}
+
 }
