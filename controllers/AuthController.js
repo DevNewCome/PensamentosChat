3bc87504e42c47598d9090ea3805b7570c69a64a
Jefferson Lira <jefferson.senac1@yahoo.com>
2022-07-26 15:29:41 -0300

    Validate password

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -5,6 +5,27 @@ module.exports = class AuthController{
   static login(req, res){
     res.render('auth/login')
   }
+
+  static async loginPost(req, res){
+    const { email, password } = req.body
+    //find user
+    const user = await User.findOne({where: {email:email}})
+
+    if(!user){
+      req.flash('message', 'Usuario não encontrado')
+      res.render('auth/login')
+      return
+    }
+    //check if password match
+    const passwordMatch = bcrypt.compareSync(password, user.password)
+    if(!passwordMatch){
+      req.flash('message', 'Senha inválida')
+      res.render('auth/login')
+      return
+    }
+  }
+
+
   static register(req, res){
     res.render('auth/register')
   }
@@ -46,12 +67,24 @@ module.exports = class AuthController{
     }
 
     try{
-      await User.create(user)
+      const createdUser = await User.create(user)
+
+      // initialize session
+      req.session.userid = createdUser.id
+
       req.flash('message', 'Cadastro realizado com sucesso')
-      res.redirect('/')
+
+      req.session.save(()=>{
+        res.redirect('/')
+      })    
     } catch (err) {
       console.log(err)
     }
    
+}
+
+static logout(req, res) {
+  req.session.destroy()
+  res.redirect('/login')
 }
 }
